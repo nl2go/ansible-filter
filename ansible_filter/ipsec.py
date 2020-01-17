a2cb71aea361bc116c1dcc13cd3ba117eaf010b1
Sanan Guliyev <sanan.quliyev@gmail.com>
2020-01-17 17:15:41 +0100

    Removed similar blocks from `ipsec` due to maintainability

--- ansible_filter/ipsec.py
+++ ansible_filter/ipsec.py
@@ -20,33 +20,15 @@ def unique_ip_pairs(hostname_list, ansible_host, hostvars, network_interfaces=No
         })
 
         remote_network_interfaces = hostvars[hostname].get("network_interfaces", {})
-        remote_ip_pairs = get_ip_pairs_from_remote_network_interfaces(hostname, ansible_host, remote_network_interfaces)
-        source_ip_pairs = get_ip_pairs_from_network_interfaces(hostname, remote_ansible_host, network_interfaces)
+        remote_ip_pairs = get_ip_pairs(hostname, ansible_host, remote_network_interfaces, "target_interface")
+        source_ip_pairs = get_ip_pairs(hostname, remote_ansible_host, network_interfaces, "source_interface")
 
         results = results + remote_ip_pairs + source_ip_pairs
 
     return results
 
 
-def get_ip_pairs_from_remote_network_interfaces(hostname, ansible_host, remote_network_interfaces):
-    ip_pairs = []
-
-    for interface_name, interface in remote_network_interfaces.items():
-        interface_ip = interface.get("ipaddress")
-        if not interface_ip:
-            continue
-
-        ip_pairs.append({
-            "target_hostname": hostname,
-            "target_interface": interface_name,
-            "first_ip": ansible_host,
-            "second_ip": interface_ip
-        })
-
-    return ip_pairs
-
-
-def get_ip_pairs_from_network_interfaces(hostname, remote_ansible_host, network_interfaces):
+def get_ip_pairs(hostname, host, network_interfaces, interface_type):
     ip_pairs = []
 
     for interface_name, interface in network_interfaces.items():
@@ -54,11 +36,14 @@ def get_ip_pairs_from_network_interfaces(hostname, remote_ansible_host, network_
         if not interface_ip:
             continue
 
-        ip_pairs.append({
+        first_ip = interface_ip if interface_type == "source_interface" else host
+        second_ip = host if interface_type == "source_interface" else interface_ip
+        ip_pair = {
             "target_hostname": hostname,
-            "source_interface": interface_name,
-            "first_ip": interface_ip,
-            "second_ip": remote_ansible_host
-        })
+            interface_type: interface_name,
+            "first_ip": first_ip,
+            "second_ip": second_ip
+        }
+        ip_pairs.append(ip_pair)
 
     return ip_pairs
